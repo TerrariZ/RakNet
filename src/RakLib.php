5515f2a91745b94598f97f90a3a7b1ef14dcdc68
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-07-07 09:49:48 +1000

    Update RakLib.php

--- src/RakLib.php
+++ src/RakLib.php
@@ -2,11 +2,11 @@
 
 /*
  * This file is part of RakLib.
- * Copyright (C) 2014-2022 PocketMine Team <https://github.com/pmmp/RakLib>
+ * Copyright (C) 2024 TerrariZ Team <https://github.com/TerrariZ/RakNet>
  *
- * RakLib is not affiliated with Jenkins Software LLC nor RakNet.
+ * RakNet is not affiliated with Jenkins Software LLC nor RakNet.
  *
- * RakLib is free software: you can redistribute it and/or modify
+ * RakNet is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -14,15 +14,15 @@
 
 declare(strict_types=1);
 
-namespace raklib;
+namespace raknet;
 
-abstract class RakLib{
+abstract class RakNet{
 	/**
 	 * Default vanilla RakNet protocol version that this library implements. Things using RakLib can override this
 	 * protocol version with something different.
 	 */
 	public const DEFAULT_PROTOCOL_VERSION = 6;
 
-	/** Regular RakNet uses 10 by default. MCPE uses 20. Configure this value as appropriate. */
+	/** Regular RakNet uses 10 by default. Configure this value as appropriate. */
 	public static int $SYSTEM_ADDRESS_COUNT = 20;
 }
